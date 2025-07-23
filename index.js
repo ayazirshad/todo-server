8cdf22c8b07ef2195916955752547aa88d50c479
Ayaz Irshad <ayazhajana180@gmaill.com>
2025-07-23 20:04:59 +0500

    added vercel file

--- index.js
+++ index.js
@@ -5,9 +5,24 @@ const taskRoutes = require("./routes/TaskRoutes");
 const cors = require("cors");
 
 const port = process.env.PORT || 8080;
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://localhost:3003",
+  //   'https://central-lending-frontend.vercel.app',
+];
+app.set("trust proxy", 1);
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
 app.use(express.json());
