1466bc37e476f766eabcd118818590e61c9b17ca
Ayaz Irshad <ayazhajana180@gmaill.com>
2025-07-23 19:41:17 +0500

    added vercel file

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const taskRoutes = require("./routes/TaskRoutes");
 const cors = require("cors");
 
 const port = process.env.PORT || 8080;
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use("/task", taskRoutes);
 
