c4be69878c5e686cae9823d3dbab4179459531ae
Ayaz Irshad <ayazhajana180@gmaill.com>
2025-07-23 23:44:33 +0500

    fixing cors

--- index.js
+++ index.js
@@ -5,29 +5,30 @@ const taskRoutes = require("./routes/TaskRoutes");
 const cors = require("cors");
 
 const port = process.env.PORT || 8080;
-// const allowedOrigins = [
-//   "http://localhost:3000",
-//   "http://localhost:3001",
-//   "http://localhost:3003",
-//   //   'https://central-lending-frontend.vercel.app',
-// ];
-// app.set("trust proxy", 1);
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
 
-app.use(cors());
+// Setup CORS
+const allowedOrigins = [
+  "http://localhost:3000",
+  //   "https://your-frontend-name.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+// app.use(cors());
 app.use(express.json());
+// app.options("*", cors());
 app.use("/task", taskRoutes);
 
 app.listen(port, () => {
