9b84c35e1ab03e8184833cc0855cc240fb9fba9c
Ayaz Irshad <ayazhajana180@gmaill.com>
2025-07-24 13:32:35 +0500

    updated options

--- index.js
+++ index.js
@@ -26,9 +26,22 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // app.use(cors());
 app.use(express.json());
-// app.options("*", cors());
 app.use("/task", taskRoutes);
 
 app.listen(port, () => {
